814fe57ad27cd378fdd240db36fbc0a3856ec6f1
Its-SaAdi <itssaadi34@gmail.com>
2023-12-23 01:21:21 +0500

    Fix Q#8 Sort

--- practice.js
+++ practice.js
@@ -62,6 +62,25 @@
 
 // 7. Remove all duplicates from an array without built-in methods.
 // let array1 = [1, 2, 3, 4, 2, 3, 5, 6, 1];
+// let array2 = [];
+// for (let i = 0; i < array1.length; i++) {
+//     if (array2.includes(array1[i])) {
+//         continue;
+//     }
+//     array2.push(array1[i]);
+// }
+// console.log(array2);
+
+// 8. Sort the array in ascending and descending without built-in methods.
+// let arrToSort = [57, 34, 66, 79, 28, 13, 84, 48, 11, 90];
+// function compare1(a, b) {
+//     return a-b;
+// }
+// function compare2(a, b) {
+//     return b-a;
+// }
+// console.log(`Ascending order: ${arrToSort.sort(compare1)}`);
+// console.log(`Descending order: ${arrToSort.sort(compare2)}`);
 
 // 9. Print all even numbers between 1 and 20 using a while loop.
 // let i = 1;
